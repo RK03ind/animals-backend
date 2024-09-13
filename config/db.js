8204e55898f7d2414a4a95cab50140753b3723e5
RK03ind <gamitech95@gmail.com>
2024-09-13 16:56:21 +0530

    Final Commit

--- config/db.js
+++ config/db.js
@@ -1,16 +1,3 @@
-import { Low } from "lowdb";
-import { JSONFile } from "lowdb/node";
-import { join, dirname } from "node:path";
-import { fileURLToPath } from "node:url";
+import { JSONFilePreset } from "lowdb/node";
 
-const __dirname = dirname(fileURLToPath(import.meta.url));
-
-const file = join(__dirname, "db.json");
-const adapter = new JSONFile(file);
-export const db = new Low(adapter);
-
-export const initDB = async () => {
-  await db.read();
-  db.data = db.data || { animals: [] };
-  await db.write();
-};
+export const db = await JSONFilePreset("db.json", { posts: [] });
--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 import express from "express";
 import animalRoutes from "./routes/animalRoutes.js";
-import { initDB } from "./config/db.js";
 
 import cors from "cors";
 
@@ -8,8 +7,6 @@ const app = express();
 const PORT = 3000;
 
 app.use(cors());
-
-initDB();
 
 app.use(express.json());
 app.use("/animals", animalRoutes);
